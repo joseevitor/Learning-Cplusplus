67e8537d4d5fd09eda6d4a4efbf526de05a1908c
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-09-23 20:23:18 -0300

    Criando_modulos

--- main.cpp
+++ main.cpp
@@ -1,55 +1,15 @@
 #include <iostream>
+#include "modulo.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    char op; // operation
-    float div;
+    int n = 5;
 
-
-
-    cout << "Type the first number!";
-    cin >> num1;
-    cout << "Type the second number!";
-    cin >> num2;
-    cout << "type the operation: ";
-    cin >> op;
-
-    switch(op)
-    {
-    case '+':
-        result = num1 +num2;
-        cout << "Sum: " << result << endl;
-        break;
-    case '-':
-        result = num1 - num2;
-        cout << "Subtraction: " << result << endl;
-        break;
-    case '*':
-        result = num1 * num2;
-        cout << "Multiplication: " << result << endl;
-        break;
-    case '/':
-        if(num2 != 0)
-        {
-            div = (float)num1 / num2;
-            cout << "Division: " << result << endl;
-        }
-        else
-        {
-            cout << "Division by zero!" << endl;
-        }
-        result = num1 / num2;
-        cout << "Division: " << result << endl;
-        break;
-
-    default:
-        cout << "Operation non-existent! Try again" << endl;
-        break;
-    }
-
+    cout << "fatorial de " << n << ": " << fatorial(n) << endl;
+    cout << "area do quadrado " << n << ": " << area_quadrado(n) << endl;
+    cout << "area do retangulo " << n << ": " << area_retangulo(n, n + 1) << endl;
 
 
     return 0;
